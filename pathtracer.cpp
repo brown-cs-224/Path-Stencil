06cb8dd7e59a5c50cf4b75fa6604a0ad1fa87e29
Tyler Schicke <tyler_schicke@brown.edu>
2018-02-08 15:44:17 -0500

    Fixed multiple objects bug

--- pathtracer.cpp
+++ pathtracer.cpp
@@ -29,6 +29,9 @@ void PathTracer::traceScene(QRgb *imageData, const Scene& scene)
 
 Vector3f PathTracer::tracePixel(int x, int y, const Scene& scene, const Matrix4f &invViewMatrix)
 {
+    if(x == 18 && y == 70) {
+        std::cout << "tesT" << std::endl;
+    }
     Vector3f p(0, 0, 0);
     Vector3f d((2.f * x / m_width) - 1, 1 - (2.f * y / m_height), -1);
     d.normalize();
@@ -42,20 +45,19 @@ Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene, const Matrix4f& 
     IntersectionInfo i;
     Ray ray(r.transform(invViewMatrix));
     if(scene.getBVH().getIntersection(ray, &i, false)) {
-        // // ** Example code for accessing the per-object material provided by the scene file **
-        const Mesh * m = static_cast<const Mesh *>(i.object);//Get the mesh that was intersected
-        const CS123SceneMaterial &material = m->getMaterialForWholeObject(); // Get per-object material
-        const Vector4f diffuse = material.cDiffuse; // Diffuse coefficient as RGBA vector
-        return diffuse.head<3>();
+          // ** Example code for accessing the per-object material provided by the scene file **
+//        const Mesh * m = static_cast<const Mesh *>(i.object);//Get the mesh that was intersected
+//        const CS123SceneMaterial &material = m->getMaterialForWholeObject(); // Get per-object material
+//        const Vector4f diffuse = material.cDiffuse; // Diffuse coefficient as RGBA vector
 
-        // // ** Example code for accessing per-face materials provided by a .mtl file **
-        //const Mesh * m = static_cast<const Mesh *>(i.object);//Get the mesh that was intersected
-        //const Triangle *t = static_cast<const Triangle *>(i.data);//Get the triangle in the mesh that was intersected
-        //const tinyobj::material_t& mat = m->getMaterial(t->getIndex());//Get the material of the triangle from the mesh
-        //const tinyobj::real_t *d = mat.diffuse;//Diffuse color, pointer to an array of 3 floats
-        //const std::string diffuseTex = mat.diffuse_texname;//Diffuse texture name
+          //** Example code for accessing per-face materials provided by a .mtl file **
+//        const Mesh * m = static_cast<const Mesh *>(i.object);//Get the mesh that was intersected
+//        const Triangle *t = static_cast<const Triangle *>(i.data);//Get the triangle in the mesh that was intersected
+//        const tinyobj::material_t& mat = m->getMaterial(t->getIndex());//Get the material of the triangle from the mesh
+//        const tinyobj::real_t *d = mat.diffuse;//Diffuse color as array of floats
+//        const std::string diffuseTex = mat.diffuse_texname;//Diffuse texture name
 
-//        return Vector3f(1, 1, 1);
+        return Vector3f(1, 1, 1);
     } else {
         return Vector3f(0, 0, 0);
     }
