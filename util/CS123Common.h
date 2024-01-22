617ebdbb4c16da456a2074620fb8e0badee98bcb
cocokaleel <cocokaleel@gmail.com>
2024-01-21 19:59:33 -0500

    Adding PFM writer function
    Should work, but is it flipping images??

--- util/CS123Common.h
+++ util/CS123Common.h
@@ -8,6 +8,8 @@
 #define __CS123COMMON_H__
 
 #include <math.h>
+#include <fstream>
+#include <vector>
 
 
 //// glu.h in different location on macs
@@ -48,6 +50,48 @@ inline bool doubleEpsEqual(double a, double b)
 
 inline Eigen::Vector4f vec3Tovec4(const Eigen::Vector3f &v, float w) {
     return Eigen::Vector4f(v.x(), v.y(), v.z(), w);
+}
+
+//following code is adapted from Microsoft's AirSim from https://github.com/microsoft/AirSim/blob/main/AirLib/include/common/common_utils/Utils.hpp
+// STUDENTS: IGNORE!! check whether machine is little endian
+inline static bool isLittleEndian()
+{
+    int intval = 1;
+    unsigned char* uval = reinterpret_cast<unsigned char*>(&intval);
+    return uval[0] == 1;
+}
+
+//STUDENTS: use this function anywhere this file is defined
+//path: the filepath of the .pfm image to be output (you must specific ".pfm")
+inline static void outputPFM(const std::string& path, int width, int height, std::vector<Eigen::Vector3f> &intensityValues, float scalef = 1) {
+    std::ofstream file(path.c_str(), std::ios::binary);
+
+            std::string bands;
+            float fvalue; // scale factor and temp value to hold pixel value
+            bands = "PF"; // rgb
+
+            // sign of scalefact indicates endianness, see pfm specs
+            if (isLittleEndian())
+                scalef = -scalef;
+
+            // insert header information
+            file << bands << "\n";
+            file << width << " ";
+            file << height << "\n";
+            file << scalef << "\n";
+
+            if (bands == "PF") { // handles 3-band image
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; ++j) {
+                        for (int p = 0; p < 3; ++p) {
+                            fvalue = intensityValues[i * width + j](p);
+                            file.write(reinterpret_cast<char*>(&fvalue), sizeof(fvalue));
+                        }
+                    }
+                }
+            }
+
+            file.close();
 }
 
 #endif
